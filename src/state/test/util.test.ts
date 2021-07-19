b6696b0310479651c98d5362d95f26726608f51b
meganrm <meganrm@gmail.com>
2021-07-19 16:34:50 -0700

    porting to jest

--- src/state/test/util.test.ts
+++ src/state/test/util.test.ts
@@ -1,45 +1,43 @@
-import { expect } from "chai";
-
 import { TypeToDescriptionMap } from "../types";
 import { makeConstant, makeReducer } from "../util";
+
+const ACTION_CONSTANT = "FAKE_CONSTANT";
+interface Action {
+  type: string;
+  arbitraryProp: boolean;
+}
+interface State {
+  flag: boolean;
+}
+
+const initialState: State = {
+  flag: false,
+};
+
+const typeToDescriptionMap: TypeToDescriptionMap = {
+  [ACTION_CONSTANT]: {
+    accepts: (action: any): action is Action =>
+      action.hasOwnProperty("arbitraryProp"),
+    perform: (state: any, action: any) => ({
+      ...state,
+      flag: action.arbitraryProp,
+    }),
+  },
+};
 
 describe("state utilities", () => {
   describe("makeConstant", () => {
     it("returns a string in the form 'APP_NAMESPACE/REDUCER/ACTION_TYPE'", () => {
       const constant = makeConstant("foo", "bar");
       const [namespace, reducer, type] = constant.split("/");
-      expect(constant).to.be.a("string");
-      expect(namespace).to.equal("VISUAL_CELL");
-      expect(reducer).to.equal("FOO");
-      expect(type).to.equal("BAR");
+      expect(constant).toBeInstanceOf("string");
+      expect(namespace).toBe("VISUAL_CELL");
+      expect(reducer).toBe("FOO");
+      expect(type).toBe("BAR");
     });
   });
 
   describe("makeReducer", () => {
-    const ACTION_CONSTANT = "FAKE_CONSTANT";
-    interface Action {
-      type: string;
-      arbitraryProp: boolean;
-    }
-    interface State {
-      flag: boolean;
-    }
-
-    const initialState: State = {
-      flag: false,
-    };
-
-    const typeToDescriptionMap: TypeToDescriptionMap = {
-      [ACTION_CONSTANT]: {
-        accepts: (action: any): action is Action =>
-          action.hasOwnProperty("arbitraryProp"),
-        perform: (state: any, action: any) => ({
-          ...state,
-          flag: action.arbitraryProp,
-        }),
-      },
-    };
-
     // typed as `any` because we know that `beforeEach` will in fact make this a valid function in every
     // assertion block
     let reducer: any;
@@ -49,17 +47,17 @@ describe("state utilities", () => {
     });
 
     it("returns a reducer function", () => {
-      expect(reducer).to.be.a("function");
+      expect(reducer).toBeInstanceOf("function");
     });
 
     it("returns given state if action type does not match key in typeToDescriptionMap", () => {
       const fakeAction = { type: "FAKE", arbitraryProp: true };
-      expect(reducer(initialState, fakeAction)).to.equal(initialState);
+      expect(reducer(initialState, fakeAction)).toBe(initialState);
     });
 
     it("returns given state if action does not pass type assertion", () => {
       const fakeAction = { type: ACTION_CONSTANT, payload: "Also fake" };
-      expect(reducer(initialState, fakeAction)).to.equal(initialState);
+      expect(reducer(initialState, fakeAction)).toBe(initialState);
     });
 
     it("returns the output of ActionDescription.perform if the type assertion passes", () => {
@@ -69,8 +67,8 @@ describe("state utilities", () => {
         realAction
       );
       const nextState = reducer(initialState, realAction);
-      expect(nextState).to.not.equal(initialState);
-      expect(nextState).to.deep.equal(expectedOutput);
+      expect(nextState).not.toBe(initialState);
+      expect(nextState).toEqual(expectedOutput);
     });
   });
 });
